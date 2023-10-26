73f43504837373905248959071345e500c548fd3
PaulaCequeira <39747099+PaulaCequeira@users.noreply.github.com>
2023-10-25 23:08:09 -0300

    Add files via upload
    Volví a subir la carpeta porque cuando la publiqué había algo que se rompió en la página de contacto. Dejé la carpeta porque se ven las participaciones de la otra integrante del grupo.

---
+++ musicaEventos(version publicada)/js/validarForm.js
@@ -0,0 +1,24 @@
+function validarForm(){
+ 
+    var nombre = document.getElementById("nombre").value.trim()
+    var apellido = document.getElementById("apellido").value.trim()
+    var tel = document.getElementById("tel").value.trim()
+    var email = document.getElementById("email").value.trim()
+    var msj = document.getElementById("msj").value.trim()
+    
+        if(nombre ==="" || apellido==="" || tel==="" || email==="" || msj===""){
+            alert("Debe completar todos los campos")
+            return false
+        }
+        var nombreTest = /^[a-zA-Z]+$/.test(nombre)
+        var apellidoTest = /^[a-zA-Z]+$/.test(apellido)
+
+        if(!nombreTest || !apellidoTest){
+            alert("Debe completar con letras el nombre y apellido")
+        }
+        if(isNaN(tel)){
+            alert("Debe completar sólo con números el tel")
+        }
+        alert("Se ha enviado la consulta correctamente")
+        return true
+} 
